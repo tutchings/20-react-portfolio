6f484bb49e35ed79081a4f8a24b77b480b258adf
tutchings <tutchings.dev@gmail.com>
2021-01-27 14:21:54 -0500

    customized favicon for website

--- src/Components/About.js
+++ src/Components/About.js
@@ -39,7 +39,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     <a href={resumeDownload} className="button" target="_blank"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
